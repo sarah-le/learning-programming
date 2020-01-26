797a96603192fb2084381aa3ba8edc5cd56bb388
Sarah Edie <sarahedie912@gmail.com>
2020-01-26 21:20:38 +1100

    Added 2_variables

---
+++ 2_variables/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+    int one = 1;
+    char s = 's';
+    float my_float = 1.0f;
+    double my_double = 4.0;
+    bool yeeee = true;
+    std::string hello_world = "Hello, world!";
+
+    std::cout << one << std::endl;
+    std::cout << s << std::endl;
+    std::cout << my_float << std::endl;
+    std::cout << my_double << std::endl;
+    std::cout << yeeee << std::endl;
+    std::cout << hello_world << std::endl;
+}

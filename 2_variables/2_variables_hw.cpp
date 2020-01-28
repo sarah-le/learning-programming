735a6edd1024ee2eda78a554ec4586372426a5e9
Sarah Edie <sarahedie912@gmail.com>
2020-01-28 17:21:35 +1100

    Completed homework for variables

---
+++ 2_variables/2_variables_hw.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+    std::string name = "Sarah Louise Edie";
+    char gender = 'F';
+    int age = 20;
+    float height = 1.5f;
+    bool like_chocolate = true;
+
+    std::cout << name << std::endl;
+    std::cout << gender << std::endl;
+    std::cout << age << std::endl;
+    std::cout << height << std::endl;
+    std::cout << like_chocolate << std::endl;
+
+}
